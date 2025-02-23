13bcd12b1e614389769cc20bfc970e31a86daf19
lidwan <contact@loayidwan.com>
2025-02-23 14:31:58 +0300

    init dashbord route

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+    return (
+      <div className=" h-screen w-screen flex justify-center items-center text-[4vw]">
+        this is the Dashboard!
+      </div>
+    );
+  }
+  

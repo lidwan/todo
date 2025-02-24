c33596f040d880759837c86391f3b4ccb4f9de79
lidwan <contact@loayidwan.com>
2025-02-24 17:06:16 +0300

    Modified styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className=" h-screen w-screen flex justify-center items-center text-[4vw]">
+    <div className=" h-screen w-[99.1vw] flex justify-center items-center text-[4vw]">
       this is the homepage!
     </div>
   );

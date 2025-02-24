88d73de3f7d0131cfd978d30d631b6c0013f1d16
lidwan <contact@loayidwan.com>
2025-02-24 23:56:20 +0300

    Modified homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className=" h-screen w-[99.1vw] flex justify-center items-center text-[4vw]">
-      this is the homepage!
+    <div className=" h-[90vh] w-[99.1vw] flex justify-center items-center md:text-[4vw] text-[8vw] text-center p-6">
+      A simple Todo web app <br/>
+      Sign-up to start using the app!
     </div>
   );
 }

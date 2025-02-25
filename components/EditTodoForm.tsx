66975086779b70123eca6facd7eaea4ec9794016
lidwan <contact@loayidwan.com>
2025-02-25 22:27:58 +0300

    removed unnecessary import

--- components/EditTodoForm.tsx
+++ components/EditTodoForm.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { string, z } from "zod";
+import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
